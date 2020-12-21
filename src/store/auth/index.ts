874e4a98e5dade5259d40792ae556e2281b7b1e3
f74981743 <bewitchsky@gmail.com>
2020-12-21 10:04:26 +0800

    feat: vuex setup

---
+++ src/store/auth/index.ts
@@ -0,0 +1,19 @@
+
+
+const auth = {
+  namespaced: true,
+  state: {
+    accessToken: '',
+    refreshToken: '',
+    expiryTime: ''
+  },
+  actions,
+  mutations,
+  getters: {
+    getAccessToken: state => state.accessToken,
+    getRefreshToken: state => state.refreshToken,
+    getExpiryTime: state => state.expiryTime,
+  },
+};
+
+export default auth;
