2130734149eea7921747fb94fc70564862f2ec30
hsuehpe <bewitchsky@gmail.com>
2021-01-06 21:48:21 +0800

    feat: nav bar

---
+++ src/components/NavBar/NavBarSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <form class="w-40 ml-4 relative text-sm">
+    <input
+      class="h-6 m-0 pr-5 pl-2 mt-2 bg-gray-500 rounded-3xl outline-none text-white dark:bg-white dark:text-black"
+      type="search"
+      placeholder="Search"
+      aria-label="Search"
+    >
+    <button class="absolute -right-2 top-0 bottom-0 h-4 my-auto mx-0 outline-none pointer-events-none">
+      <Icon icon="carbon:search" />
+    </button>
+  </form>
+</template>
---
+++ src/components/NavBar/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="h-10 flex bg-white dark:bg-black">
+    <nav-bar-history />
+    <nav-bar-search />
+  </nav>
+</template>
+
+<script setup lang="ts">
+import NavBarHistory from './NavBarHistory.vue'
+import NavBarSearch from './NavBarSearch.vue'
+
+</script>
