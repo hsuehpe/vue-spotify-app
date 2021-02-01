be4c3ccbc0e838de1c38520118906be780de83cd
hsuehpe <bewitchsky@gmail.com>
2021-02-01 22:51:09 +0800

    feat: add entity header component

---
+++ src/components/EntityHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="py-4">
+    <h2 :class="titleCls">
+      {{ title }}
+    </h2>
+  </div>
+</template>
+
+<script setup="props" lang="ts">
+import { defineProps, computed } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+
+})
+
+const titleCls = computed(() => (props.small) ? 'mt-8 mb-0 mx-4 text-xl' : 'mt-8 mb-0 mx-4 text-3xl')
+</script>
