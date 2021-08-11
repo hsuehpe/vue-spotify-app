389924e70027f4d4aaa250e4c694482c32b06b43
hsuehpe <bewitchsky@gmail.com>
2021-08-11 09:50:35 +0800

    update backend base URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const backendBaseUrl = 'http://localhost:8080/vue-spotify-app/v1/'
+export const backendBaseUrl = process.env.NODE_ENV === 'production' ? 'https://vue-spotify-app-gbcbfjyvfa-de.a.run.app/vue-spotify-app/v1/' : 'http://localhost:8080/vue-spotify-app/v1/'
 export const spotifyBaseUrl = 'https://api.spotify.com/v1/'
